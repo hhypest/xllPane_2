4195de56f453254419e6f6a12175be9a3597831a
hhypest <hypest3@yandex.ru>
2022-05-09 16:36:40 +0300

    Ex

--- xllPane 2/RibbCls.cs
+++ xllPane 2/RibbCls.cs
@@ -794,7 +794,7 @@ namespace xllPane_2
             }
         }
 
-        public void OnExTableClick(IRibbonControl control)
+        public void OnExTableClickAsync(IRibbonControl control)
         {
             Application app = ExcelDnaUtil.Application as Application;
 
